cd6364a1d91db7e40637417578c73d0b8ad8f6c0
medaziz2002 <azizhassine673@gmail.com>
2025-01-30 10:23:43 +0100

    Mon premier rendu visuel

--- src/Controller/FactController.php
+++ src/Controller/FactController.php
@@ -4,12 +4,16 @@ namespace App\Controller;
 
 use App\Utils\MathUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class FactController extends AbstractController{
+
+
+
     #[Route('/fact/{n}', name: 'app_fact',requirements:['n'=>'\d+'])]
-    public function index(int $n): Response
+    public function fact(int $n): Response
     {
         $result =MathUtils::factorielle($n);
         return new Response("{$n}!={$result}");
@@ -23,6 +27,42 @@ final class FactController extends AbstractController{
         return new Response("C{$n} {$p}={$result}");
     }
 
+    
+    #[Route('/',name:'home')]
+    public function index(Request $request):Response
+    {
+      
+        $n=$request->query->get('n');
+        $p=$request->query->get('p');
+        $result=null;
+        $calculationType = null;
+        if($n!=null && is_numeric($n))
+        {
+            $n=(int) $n;
+            $result=MathUtils::factorielle($n);
+            $calculationType = "Factorielle de {$n}";
+        }
 
+        if($n!=null && is_numeric($n) && $p!=null && is_numeric($p))
+        {
 
+            $n=(int) $n;
+            $p=(int) $p;
+       
+            if ($p > $n) {
+                $result = "Erreur : p ne peut pas être supérieur à n";
+            } else {
+                $result = MathUtils::combinaison($n, $p);
+                $calculationType = "Combinaison C($n, $p)";
+            }
+
+      
+        }
+        return $this->render('index.html.twig', [
+            'result' => $result,
+            'calculationType' => $calculationType,
+            'n' => $n,
+            'p' => $p
+        ]);
+    }
 }
